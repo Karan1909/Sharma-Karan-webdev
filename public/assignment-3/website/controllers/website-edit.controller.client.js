26e4f3eb4fa8f28234077fa7050de8d83f06f3fd
Karan1909 <karan1909@ccs.neu.edu>
2017-02-16 13:05:13 -0500

    Update 1

--- public/assignment-3/website/controllers/website-edit.controller.client.js
+++ public/assignment-3/website/controllers/website-edit.controller.client.js
@@ -3,12 +3,27 @@
         .module("WebAppMaker")
         .controller("WebsiteEditController",WebsiteEditController);
 
-    function WebsiteEditController($routeParams, WebsiteService) {
+    function WebsiteEditController($routeParams, WebsiteService,$location) {
         var vm = this;
         vm.userId = $routeParams.uid;
         vm.websiteId=$routeParams.wid;// wid we get from the config
-        vm.websites = WebsiteService.findAllWebsitesForUser(vm.userId);
+        vm.websites = WebsiteService.findWebsitesForUser(vm.userId);
         vm.website=WebsiteService.findWebsiteById(vm.websiteId);
+
+        vm.updateWebsite=updateWebsite;
+        vm.deleteWebsite=deleteWebsite;
+
+        function updateWebsite(websites) {
+            WebsiteService.updateWebsite(vm.websiteId,websites);
+            $location.url("/user/"+vm.userId+"/website/");
+        }
+
+        function deleteWebsite() {
+            WebsiteService.deleteWebsite(vm.websiteId);
+            $location.url("/user/"+vm.userId+"/website/");
+
+        }
+
 
 
 
