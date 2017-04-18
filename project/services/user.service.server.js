b4e6f1dca35c1ed01bc3748eb8591a1f99cde0a0
Karan1909 <karan1909@ccs.neu.edu>
2017-04-18 15:01:41 -0400

    c1

--- project/services/user.service.server.js
+++ project/services/user.service.server.js
@@ -40,7 +40,7 @@ module.exports = function (app,model) {
     //     console.log("login with google");
     // });
 
-    app.get('/auth/google/callback',
+    app.get('/callback',
         passport.authenticate('google', {
             successRedirect: '/project/#/user/viewProfile',
             failureRedirect: '/#'
